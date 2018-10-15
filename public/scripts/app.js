cbae5cc66e403144c18fdef29f2b1044fd827675
Jérémy Kervran <kervran.jeremy@gmail.com>
2018-10-15 19:40:22 +0200

    commit home Section 3 Session 12

---
+++ public/scripts/app.js
@@ -0,0 +1,84 @@
+'use strict';
+
+console.log('App.js is running!');
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  app.options.length > 0 ? React.createElement(
+    'p',
+    null,
+    'Here are your options'
+  ) : React.createElement(
+    'p',
+    null,
+    'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      app.options[0]
+    ),
+    React.createElement(
+      'li',
+      null,
+      app.options[1]
+    )
+  )
+);
+
+var user = {
+  name: 'Andrew',
+  age: 19,
+  location: 'New York'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
